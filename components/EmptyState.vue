c527fa5b1ed55cd99c32d2225544fa5093db81b4
habibmac <mcrury@gmail.com>
2024-08-21 14:38:56 +0700

    feat: add tanstack/vue-query

---
+++ components/EmptyState.vue
@@ -0,0 +1,58 @@
+<script setup lang="ts">
+defineProps({
+	title: {
+		type: String,
+		default: 'No data found',
+	},
+	description: {
+		type: String,
+		required: false,
+	},
+	cta: {
+		type: Object,
+		default: () => ({
+			label: 'Reset Filters',
+			to: '',
+		}),
+	},
+});
+</script>
+<template>
+	<div class="m-auto my-4 max-w-2xl">
+		<div class="px-4 text-center">
+			<div
+				class="mb-4 inline-flex h-16 w-16 items-center justify-center rounded-full bg-gradient-to-t from-slate-200 to-slate-100 dark:from-slate-700 dark:to-slate-800"
+			>
+				<svg class="h-6 w-5 fill-current" viewBox="0 0 20 24">
+					<path
+						class="text-slate-500 dark:text-slate-600"
+						d="M10 10.562l9-5-8.514-4.73a1 1 0 00-.972 0L1 5.562l9 5z"
+					/>
+					<path
+						class="text-slate-300 dark:text-slate-400"
+						d="M9 12.294l-9-5v10.412a1 1 0 00.514.874L9 23.294v-11z"
+					/>
+					<path
+						class="text-slate-400 dark:text-slate-500"
+						d="M11 12.294v11l8.486-4.714a1 1 0 00.514-.874V7.295l-9 4.999z"
+					/>
+				</svg>
+			</div>
+			<h2
+				class="mb-2 text-xl font-bold text-slate-500 dark:text-slate-100"
+			>
+				{{ title }}
+			</h2>
+			<p class="mb-5 text-sm" v-if="description">
+				{{ description }}
+			</p>
+
+			<NuxtLink
+				class="btn bg-blue-500 text-slate-50 hover:bg-blue-600 dark:bg-blue-600 dark:text-slate-100 dark:hover:bg-blue-700"
+				:to="cta.to"
+			>
+				<span class="text-sm font-medium">{{ cta.label }}</span>
+			</NuxtLink>
+		</div>
+	</div>
+</template>
---
+++ composables/useEvents.ts
@@ -0,0 +1,96 @@
+import { computed, ref, type Ref } from 'vue';
+import { useQuery, keepPreviousData } from '@tanstack/vue-query';
+
+interface GetDataParams {
+    evtStatus?: Ref<string>;
+    page?: Ref<number>;
+    perPage?: Ref<number>;
+}
+
+export const useEvents = ({
+    evtStatus = ref('all'),
+    page = ref(1),
+    perPage = ref(10),
+}: GetDataParams = {}) => {
+
+    const { $galantisApi } = useNuxtApp();
+    const getData = async (
+        evtStatus: Ref<string>,
+        page: Ref<number>,
+        perPage: Ref<number>,
+        signal: AbortSignal,
+    ) => {
+        return $galantisApi
+            .get('/events', {
+                params: {
+                    page: page.value,
+                    per_page: perPage.value,
+                    evt_status: evtStatus.value,
+                },
+                signal,
+            })
+            .then((response) => {
+                return {
+                    events: response.data.data,
+                    pagination: response.data.pagination,
+                };
+            })
+            .catch((error) => {
+                throw new Error(error);
+            });
+    };
+
+    const { isLoading, isError, error, data, isPlaceholderData } = useQuery({
+        queryKey: ['events', page, perPage, evtStatus],
+        queryFn: ({ signal }) => getData(evtStatus, page, perPage, signal),
+        placeholderData: keepPreviousData,
+        staleTime: 1000 * 60 * 5, // 5 minutes
+    });
+
+    const events = computed(() => data?.value?.events);
+    const pagination = computed(() => data?.value?.pagination);
+
+    // Pagination
+    const maxPage = computed(() => pagination.value?.total_pages);
+
+    const setPage = (newPage: number) => {
+        page.value = newPage;
+    };
+
+    const setPerPage = (newPerPage: number) => {
+        perPage.value = newPerPage;
+    };
+
+    const setEvtStatus = (newEvtStatus: string) => {
+        evtStatus.value = newEvtStatus;
+    };
+
+    const prevPage = () => {
+        if (page.value > 1) {
+            setPage(page.value - 1);
+        }
+    };
+
+    const nextPage = () => {
+        if (page.value < maxPage.value) {
+            setPage(page.value + 1);
+        }
+    };
+
+    return {
+        isLoading,
+        isError,
+        error,
+        events,
+        isPlaceholderData,
+        pagination,
+        page,
+        perPage,
+        maxPage,
+        prevPage,
+        nextPage,
+        setPage,
+        setPerPage,
+        setEvtStatus,
+    };
+};
