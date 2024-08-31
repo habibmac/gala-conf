c6db72115f46c2cc159b6ea1a4b8aec0e17fbaa7
habibmac <mcrury@gmail.com>
2024-08-31 17:59:19 +0700

    CI/CI: fix correct ecosystem

---
+++ ecosystem.config.cjs
@@ -0,0 +1,16 @@
+module.exports = {
+  apps: [
+    {
+      name: "live.galanesia.com",
+      exec_mode: "cluster",
+      instances: "max",
+      script: "./server/index.mjs",
+      env: {
+        NODE_ENV: "production",
+      },
+      env_production: {
+        NODE_ENV: "production",
+      },
+    },
+  ],
+};
