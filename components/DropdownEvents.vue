3b40888aec3ad55dea303fa722951a7991363bf1
habibmac <mcrury@gmail.com>
2024-09-01 11:54:25 +0700

    fix: dropdown event's UI

--- components/DropdownEvents.vue
+++ components/DropdownEvents.vue
@@ -142,14 +142,14 @@ watch(search, (newValue) => {
               >
                 <div class="sticky z-10 top-0 pointer-events-none">
                   <div
-                    class="p-3 bg-white dark:bg-slate-950 relative pointer-events-auto"
+                    class="px-2 py-3 bg-white dark:bg-slate-950 relative pointer-events-auto"
                   >
                     <div
-                      class="absolute top-1/2 -translate-y-1/2 left-6 pointer-events-none"
+                      class="absolute top-1/2 -translate-y-1/2 left-5 pointer-events-none"
                     >
                       <SpinnerDots
                         v-if="isRefetching"
-                        class="h-7 w-7 text-slate-400 dark:text-slate-500"
+                        class="h-5 w-5 text-slate-400 dark:text-slate-500"
                       />
                       <Icon
                         v-else
@@ -159,7 +159,7 @@ watch(search, (newValue) => {
                     </div>
                     <Input
                       type="search"
-                      class="h-8 px-4 pl-12 border-b border-slate-200 dark:border-slate-700"
+                      class="h-8 px-2 pl-10 border-b border-slate-200 dark:border-slate-700"
                       placeholder="Search events..."
                       v-model="search"
                     />
@@ -169,7 +169,7 @@ watch(search, (newValue) => {
                   ></div>
                 </div>
 
-                <ul class="-mt-4" v-if="events.length > 0">
+                <ul class="-mt-4 flex flex-col px-2 gap-1" v-if="events.length > 0">
                   <ListboxOption
                     :key="event.id"
                     :value="event"
@@ -179,7 +179,7 @@ watch(search, (newValue) => {
                       isSelected(event.id)
                         ? 'bg-emerald-100 dark:bg-emerald-900/40'
                         : 'hover:bg-slate-100 dark:hover:bg-slate-900/80',
-                      'rounded-md flex w-full cursor-default select-none items-center space-x-2 px-3 py-3 text-slate-700 dark:text-slate-300',
+                      'rounded-md flex w-full cursor-default select-none items-center space-x-2 px-3 py-2 text-slate-700 dark:text-slate-300',
                     ]"
                     @click="handleselectEvent(event.id)"
                   >
@@ -190,7 +190,7 @@ watch(search, (newValue) => {
                       <EventCover
                         :src="event?.logo"
                         :title="event.title"
-                        class="h-10 w-10 relative shrink-0 overflow-hidden rounded-sm bg-blue-900/20"
+                        class="h-10 w-10 relative shrink-0 overflow-hidden"
                       />
                       <p
                         :class="[
@@ -229,12 +229,7 @@ watch(search, (newValue) => {
                         cn('rounded-l-md', { 'text-slate-500': page === 1 })
                       "
                     >
-                      <svg class="h-6 w-6" viewBox="0 0 24 24">
-                        <path
-                          fill="currentColor"
-                          d="M14.121 17.243a.997.997 0 0 1-.707-.293l-4.242-4.243a1 1 0 0 1 0-1.414l4.242-4.243a1 1 0 0 1 1.414 1.414L11.293 12l3.535 3.536a1 1 0 0 1-.707 1.707"
-                        />
-                      </svg>
+                      <Icon class="h-4 w-4" icon="heroicons-outline:chevron-left" />
                     </Button>
                     <Button
                       variant="ghost"
@@ -245,12 +240,7 @@ watch(search, (newValue) => {
                         cn('rounded-l-md', { 'text-slate-500': page === 1 })
                       "
                     >
-                      <svg class="h-6 w-6" viewBox="0 0 24 24">
-                        <path
-                          fill="currentColor"
-                          d="M9.879 17.243a1 1 0 0 1-.707-1.707L12.707 12L9.172 8.464a1 1 0 0 1 1.414-1.414l4.242 4.243a1 1 0 0 1 0 1.414l-4.242 4.243a.997.997 0 0 1-.707.293"
-                        />
-                      </svg>
+                      <Icon class="h-4 w-4" icon="heroicons-outline:chevron-right" />
                     </Button>
                   </div>
                 </div>
