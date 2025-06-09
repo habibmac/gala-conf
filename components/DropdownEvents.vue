771426308012ff34e856badca1341dd6396d986f
habibmac <mcrury@gmail.com>
2025-06-10 02:29:51 +0700

    feat: add scroll lock on mobile in reg details & event dropdown

--- components/DropdownEvents.vue
+++ components/DropdownEvents.vue
@@ -15,6 +15,7 @@ import { Input } from './ui/input';
 const router = useRouter();
 const selectedEventId = ref<string | null>(null);
 const search = ref<string>(''); // Provide a default value for search
+const isDropdownOpen = ref(false);
 
 // Fetch events
 const { error, event, isError, isLoading } = useEvent();
@@ -49,9 +50,34 @@ const debouncedSearch = useDebounceFn((newValue: string) => {
   }
 }, 300);
 
+// Function to handle scroll locking
+const toggleScrollLock = (lock: boolean) => {
+  if (typeof document === 'undefined')
+    return;
+
+  if (lock) {
+    document.body.style.overflow = 'hidden';
+    document.body.style.paddingRight = `${window.innerWidth - document.documentElement.clientWidth}px`;
+  }
+  else {
+    document.body.style.overflow = '';
+    document.body.style.paddingRight = '';
+  }
+};
+
 watch(search, (newValue) => {
   console.warn('Search value changed:', newValue);
   debouncedSearch(newValue);
+});
+
+// Watch for dropdown state changes
+watch(isDropdownOpen, (isOpen) => {
+  toggleScrollLock(isOpen);
+});
+
+// Cleanup on component unmount
+onUnmounted(() => {
+  toggleScrollLock(false);
 });
 </script>
 
@@ -70,9 +96,9 @@ watch(search, (newValue) => {
               ⚠️ Cannot fetch events, please try again later.
             </div>
             <div v-else-if="event" class="flex w-full items-center justify-between space-x-1 pl-4 pr-10 md:pl-2">
-              <div class="flex grow items-center space-x-2 sm:space-x-3">
+              <div class="flex items-center space-x-2 sm:space-x-3">
                 <EventCover class="size-6" :src="event.logo" :title="event.title" />
-                <div class="flex w-px items-center font-medium leading-tight sm:w-auto">
+                <div class="hidden w-px items-center font-medium leading-tight sm:flex sm:w-auto">
                   <span class="line-clamp-2">{{ event.title }}</span>
                 </div>
               </div>
@@ -106,109 +132,116 @@ watch(search, (newValue) => {
           leave-from-class="opacity-100"
           leave-to-class="opacity-0"
         >
-          <ListboxOptions
-            class="absolute left-0 mt-2.5 flex h-[calc(100dvh-64px)] w-full flex-col overflow-hidden border bg-white text-sm shadow-lg ring-black/5 focus:outline-none dark:border-slate-800 dark:bg-slate-950 sm:left-auto sm:mt-1 sm:h-auto sm:min-h-min sm:w-96 sm:rounded-lg md:max-h-[calc(100dvh-64px)]"
-          >
-            <template v-if="isLoading">
-              <li class="flex w-full justify-center py-10">
-                <SpinnerDots class="size-10 text-blue-700" />
-              </li>
-            </template>
-
-            <template v-else-if="isError">
-              <li class="flex w-full justify-center py-10">
-                {{ error }}
-              </li>
-            </template>
-
-            <template v-else-if="events">
-              <div
-                class="scroll-area relative grid grow auto-rows-min grid-cols-1 gap-0.5 overflow-y-auto overscroll-y-contain pb-10 sm:pb-4 md:max-h-96"
-              >
-                <div class="pointer-events-none sticky top-0 z-10">
-                  <div class="pointer-events-auto relative bg-white px-2 py-3 dark:bg-slate-950">
-                    <div class="pointer-events-none absolute left-5 top-1/2 -translate-y-1/2">
-                      <SpinnerDots v-if="isRefetching" class="size-5 text-slate-400 dark:text-slate-500" />
-                      <Icon v-else class="size-5 text-slate-400 dark:text-slate-500" icon="heroicons-outline:search" />
-                    </div>
-                    <Input
-                      v-model="search"
-                      type="search"
-                      class="h-8 border-b border-slate-200 px-2 pl-10 dark:border-slate-700"
-                      placeholder="Search events..."
-                    />
-                  </div>
-                  <div class="h-3 bg-gradient-to-b from-white dark:from-slate-950" />
-                </div>
-
-                <ul v-if="events.length > 0" class="-mt-4 flex flex-col gap-1 px-2">
-                  <ListboxOption
-                    v-for="evt in events"
-                    :key="evt.id"
-                    :value="evt"
-                    as="li"
-                    class="flex w-full cursor-default select-none items-center space-x-2 rounded-md px-3 py-2 text-slate-700 dark:text-slate-300" :class="[
-                      isSelected(evt.id)
-                        ? 'bg-emerald-100 dark:bg-emerald-900/40'
-                        : 'hover:bg-slate-100 dark:hover:bg-slate-900/80',
-                    ]"
-                    @click="handleselectEvent(evt.id)"
-                  >
-                    <template v-if="isLoading">
-                      <SpinnerDots class="mx-auto size-10 text-blue-50" />
-                    </template>
-                    <template v-else-if="evt">
-                      <EventCover :src="evt?.logo" :title="evt.title" />
-                      <p class="line-clamp-2 pr-5 leading-tight" :class="[isSelected(evt.id) ? 'font-bold' : 'font-medium']">
-                        {{ evt.title }}
-                      </p>
-                      <span
-                        v-if="isSelected(evt.id)"
-                        class="absolute right-5 flex size-2.5 items-center rounded-full bg-emerald-400"
-                      />
-                    </template>
-                  </ListboxOption>
-                </ul>
-                <div v-else class="flex w-full justify-center py-10">
-                  No events found
-                </div>
-              </div>
-              <template v-if="events.length > 0">
-                <div
-                  class="pointer-events-none absolute bottom-12 z-20 h-20 w-full bg-gradient-to-t from-white dark:from-slate-950 sm:hidden"
-                />
-                <div
-                  class="flex h-12 w-full shrink-0 items-center justify-end border-t border-slate-200 p-3 dark:border-slate-800 sm:col-span-2"
-                >
-                  <div class="flex space-x-px">
-                    <Button
-                      v-if="page > 1"
-                      variant="ghost"
-                      size="icon"
-                      :class="cn('rounded-l-md', { 'text-slate-500': page === 1 })"
-                      @click="prevPage"
-                    >
-                      <Icon class="size-4" icon="heroicons-outline:chevron-left" />
-                    </Button>
-                    <Button
-                      v-if="page < maxPage"
-                      variant="ghost"
-                      size="icon"
-                      :class="cn('rounded-l-md', { 'text-slate-500': page === 1 })"
-                      @click="nextPage"
-                    >
-                      <Icon class="size-4" icon="heroicons-outline:chevron-right" />
-                    </Button>
-                  </div>
-                </div>
+          <div v-if="open">
+            <ListboxOptions
+              class="absolute left-0 z-50 mt-2.5 flex h-[calc(100dvh-64px)] w-full flex-col overflow-hidden border bg-white text-sm shadow-lg ring-black/5 focus:outline-none dark:border-slate-800 dark:bg-slate-950 sm:left-auto sm:mt-1 sm:h-auto sm:min-h-min sm:w-96 sm:rounded-lg md:max-h-[calc(100dvh-64px)]"
+            >
+              <template v-if="isLoading">
+                <li class="flex w-full justify-center py-10">
+                  <SpinnerDots class="size-10 text-blue-700" />
+                </li>
               </template>
-            </template>
-            <template v-else>
-              <li class="flex w-full justify-center py-10">
-                No events found
-              </li>
-            </template>
-          </ListboxOptions>
+              <template v-else-if="isError">
+                <li class="flex w-full justify-center py-10">
+                  {{ error }}
+                </li>
+              </template>
+              <template v-else-if="events">
+                <div
+                  class="scroll-area relative grid grow auto-rows-min grid-cols-1 gap-0.5 overflow-y-auto overscroll-y-contain pb-10 sm:pb-4 md:max-h-96"
+                >
+                  <div class="pointer-events-none sticky top-0 z-10">
+                    <div class="pointer-events-auto relative bg-white px-2 py-3 dark:bg-slate-950">
+                      <div class="pointer-events-none absolute left-5 top-1/2 -translate-y-1/2">
+                        <SpinnerDots v-if="isRefetching" class="size-5 text-slate-400 dark:text-slate-500" />
+                        <Icon
+                          v-else
+                          class="size-5 text-slate-400 dark:text-slate-500"
+                          icon="heroicons-outline:search"
+                        />
+                      </div>
+                      <Input
+                        v-model="search"
+                        type="search"
+                        class="h-8 border-b border-slate-200 px-2 pl-10 dark:border-slate-700"
+                        placeholder="Search events..."
+                      />
+                    </div>
+                    <div class="h-3 bg-gradient-to-b from-white dark:from-slate-950" />
+                  </div>
+                  <ul v-if="events.length > 0" class="-mt-4 flex flex-col gap-1 px-2">
+                    <ListboxOption
+                      v-for="evt in events"
+                      :key="evt.id"
+                      :value="evt"
+                      as="li"
+                      class="flex w-full cursor-default select-none items-center space-x-2 rounded-md px-3 py-2 text-slate-700 dark:text-slate-300"
+                      :class="[
+                        isSelected(evt.id)
+                          ? 'bg-emerald-100 dark:bg-emerald-900/40'
+                          : 'hover:bg-slate-100 dark:hover:bg-slate-900/80',
+                      ]"
+                      @click="handleselectEvent(evt.id)"
+                    >
+                      <template v-if="isLoading">
+                        <SpinnerDots class="mx-auto size-10 text-blue-50" />
+                      </template>
+                      <template v-else-if="evt">
+                        <EventCover :src="evt?.logo" :title="evt.title" />
+                        <p
+                          class="line-clamp-2 pr-5 leading-tight"
+                          :class="[isSelected(evt.id) ? 'font-bold' : 'font-medium']"
+                        >
+                          {{ evt.title }}
+                        </p>
+                        <span
+                          v-if="isSelected(evt.id)"
+                          class="absolute right-5 flex size-2.5 items-center rounded-full bg-emerald-400"
+                        />
+                      </template>
+                    </ListboxOption>
+                  </ul>
+                  <div v-else class="flex w-full justify-center py-10">
+                    No events found
+                  </div>
+                </div>
+                <template v-if="events.length > 0">
+                  <div
+                    class="pointer-events-none absolute bottom-12 z-20 h-20 w-full bg-gradient-to-t from-white dark:from-slate-950 sm:hidden"
+                  />
+                  <div
+                    class="flex h-12 w-full shrink-0 items-center justify-end border-t border-slate-200 p-3 dark:border-slate-800 sm:col-span-2"
+                  >
+                    <div class="flex space-x-px">
+                      <Button
+                        v-if="page > 1"
+                        variant="ghost"
+                        size="icon"
+                        :class="cn('rounded-l-md', { 'text-slate-500': page === 1 })"
+                        @click="prevPage"
+                      >
+                        <Icon class="size-4" icon="heroicons-outline:chevron-left" />
+                      </Button>
+                      <Button
+                        v-if="page < maxPage"
+                        variant="ghost"
+                        size="icon"
+                        :class="cn('rounded-l-md', { 'text-slate-500': page === 1 })"
+                        @click="nextPage"
+                      >
+                        <Icon class="size-4" icon="heroicons-outline:chevron-right" />
+                      </Button>
+                    </div>
+                  </div>
+                </template>
+              </template>
+              <template v-else>
+                <li class="flex w-full justify-center py-10">
+                  No events found
+                </li>
+              </template>
+            </ListboxOptions>
+          </div>
         </Transition>
       </Listbox>
     </ClientOnly>
--- components/partials/registrations/RegDetails.vue
+++ components/partials/registrations/RegDetails.vue
@@ -1,9 +1,13 @@
 <script setup lang="ts">
 import { Icon } from '@iconify/vue';
 import { useQuery, useQueryClient } from '@tanstack/vue-query';
+import { useScrollLock } from '@vueuse/core';
 import { onMounted, onUnmounted, ref, toRef, watchEffect } from 'vue';
 
+import CopyButton from '@/components/CopyButton.vue';
+import EmptyState from '@/components/EmptyState.vue';
 import PayMethodLogo from '@/components/partials/registrations/PayMethodLogo.vue';
+import { Badge } from '@/components/ui/badge';
 import { formatCurrency, formatDate, getCountryFlagWithName } from '@/utils';
 import { getStatusInfo } from '@/utils/status-map';
 
@@ -34,6 +38,8 @@ const closePanel = () => {
 const loading = ref(false);
 const panelContent = ref<HTMLElement | null>(null);
 const closeBtn = ref<HTMLElement | null>(null);
+
+const isLocked = ref(false);
 
 // close on click outside
 const clickHandler = (event: MouseEvent) => {
@@ -79,7 +85,7 @@ const getData = async (signal: AbortSignal) => {
 };
 
 // Fetching data with useQuery and TypeScript for response typing
-const { data, error, isError, isRefetching, refetch } = useQuery({
+const { data, isError, isRefetching, refetch } = useQuery({
   // After the data is fetched, set loading to false
   enabled: !!props.evtId && !!props.regId,
   placeholderData: {},
@@ -89,6 +95,8 @@ const { data, error, isError, isRefetching, refetch } = useQuery({
 
 // Watch for changes in route or events data to update the selected event
 watchEffect(() => {
+  isLocked.value = props.regDetailsOpen;
+
   if (props.regId) {
     // Reset data
     refetch();
@@ -96,6 +104,14 @@ watchEffect(() => {
 });
 
 onMounted(() => {
+  if (import.meta.client) {
+    const scrollLock = useScrollLock(document.body);
+
+    watchEffect(() => {
+      scrollLock.value = props.regDetailsOpen;
+    });
+  }
+
   document.addEventListener('click', clickHandler);
   document.addEventListener('keydown', keyHandler);
 });
@@ -109,20 +125,18 @@ onUnmounted(async () => {
 
 <template>
   <Transition
-    enter-active-class="transition duration-100 ease-in-out"
-    enter-from-class="translate-x-[20%] opacity-0"
-    enter-to-class="translate-x-0 opacity-100"
-    leave-active-class="transition duration-200"
-    leave-from-class="translate-x-0 "
-    leave-to-class="translate-x-[20%] opacity-0"
+    enter-active-class="transition duration-100 ease-in-out" enter-from-class="translate-x-[20%] opacity-0"
+    enter-to-class="translate-x-0 opacity-100" leave-active-class="transition duration-200"
+    leave-from-class="translate-x-0 " leave-to-class="translate-x-[20%] opacity-0"
   >
-    <div v-show="regDetailsOpen" ref="panelContent" class="absolute inset-0 z-30 sm:left-auto sm:block">
+    <div v-show="regDetailsOpen" class="absolute inset-0 z-30 sm:left-auto sm:block">
+      <div class="fixed inset-0 z-20 bg-black/50 sm:hidden" @click.stop="$emit('close-reg-details')" />
       <div
-        class="no-scrollbar sticky top-16 h-[calc(100dvh-64px)] w-full shrink-0 overflow-y-auto overflow-x-hidden border-l border-border bg-muted sm:w-[390px]"
+        ref="panelContent"
+        class="no-scrollbar fixed inset-0 z-30 overflow-y-auto overflow-x-hidden border-l border-border bg-slate-50 dark:bg-slate-800 sm:sticky sm:top-16 sm:h-[calc(100dvh-64px)] sm:w-[390px]"
       >
         <button
-          ref="closeBtn"
-          class="group absolute right-0 top-0 mr-6 mt-6 p-2"
+          ref="closeBtn" class="group absolute right-0 top-0 mr-6 mt-6 p-2"
           @click.stop="$emit('close-reg-details')"
         >
           <Icon icon="iconamoon:close-thin" class="size-6" />
@@ -137,7 +151,15 @@ onUnmounted(async () => {
             <Skeleton class="h-20 bg-muted-foreground/10 p-4" />
           </div>
           <div v-else-if="isError">
-            {{ error ? error.message : 'Error' }}
+            <EmptyState
+              title="An error occurred" description="We couldn't load the registration details.
+              Please contact support or try again later."
+              :img="{ src: '/images/empty-state/no-event.svg', alt: 'Error' }" description-class="text-sm" :cta2="{
+                icon: 'heroicons:arrow-path-solid',
+                label: 'Refresh',
+                action: refetch,
+              }"
+            />
           </div>
           <div v-else-if="data" class="mx-auto max-w-sm lg:max-w-none">
             <div class="mb-1 text-center font-semibold text-slate-800 dark:text-slate-100" />
@@ -185,11 +207,25 @@ onUnmounted(async () => {
                       <Qrcode :value="data.reg_url_link" />
                     </ClientOnly>
                   </div>
-                  <span class="number font-mono font-semibold tabular-nums">{{ data.code }}</span>
+                  <div class="relative">
+                    <Badge
+                      variant="outline"
+                      class="text-md number rounded-full border border-muted-foreground font-medium tabular-nums"
+                    >
+                      {{ data.code }}
+                    </Badge>
+                    <!-- Copy btn -->
+                    <CopyButton
+                      :value="data.code" text="Copy code"
+                      button-class="absolute -right-8 top-1/2 -translate-y-1/2 text-muted-foreground hover:text-slate-800 dark:hover:text-slate-100"
+                      side="top"
+                    />
+                  </div>
                 </div>
                 <div class="flex justify-between space-x-1">
                   <span class="">Age/Gender</span>
-                  <span class="text-right font-medium text-slate-700 dark:text-slate-100">{{ data.age }} / {{ data.gender }}</span>
+                  <span class="text-right font-medium text-slate-700 dark:text-slate-100">{{ data.age }} / {{
+                    data.gender }}</span>
                 </div>
                 <div class="flex justify-between space-x-1">
                   <span class="">Email:</span>
@@ -272,8 +308,7 @@ onUnmounted(async () => {
             <div class="mt-6 flex items-center space-x-3">
               <div class="w-1/2">
                 <a
-                  :href="data.invoice_url"
-                  target="_blank"
+                  :href="data.invoice_url" target="_blank"
                   class="btn w-full border border-slate-100 bg-white text-sm font-medium text-slate-600 hover:border-slate-300 dark:border-slate-700 dark:bg-slate-800 dark:text-slate-300 dark:hover:border-slate-600"
                 >
                   <svg xmlns="http://www.w3.org/2000/svg" class="size-6 shrink-0" viewBox="0 0 24 24">
@@ -282,8 +317,7 @@ onUnmounted(async () => {
                       d="M13.5 9.75a.75.75 0 0 0-.75-.75h-6a.75.75 0 0 0 0 1.5h6a.75.75 0 0 0 .75-.75m-1 3a.75.75 0 0 0-.75-.75h-5a.75.75 0 1 0 0 1.5h5a.75.75 0 0 0 .75-.75m.25 2.25a.75.75 0 1 1 0 1.5h-6a.75.75 0 0 1 0-1.5z"
                     />
                     <path
-                      fill="currentColor"
-                      fill-rule="evenodd"
+                      fill="currentColor" fill-rule="evenodd"
                       d="M6 21.75h13A2.75 2.75 0 0 0 21.75 19v-5.5a.75.75 0 0 0-.75-.75h-3.25V4.943c0-1.423-1.609-2.251-2.767-1.424l-.175.125a2.26 2.26 0 0 1-2.622-.004a3.77 3.77 0 0 0-4.372 0a2.26 2.26 0 0 1-2.622.004l-.175-.125c-1.158-.827-2.767 0-2.767 1.424V18A3.75 3.75 0 0 0 6 21.75M8.686 4.86a2.27 2.27 0 0 1 2.628 0a3.76 3.76 0 0 0 4.366.005l.175-.125a.25.25 0 0 1 .395.203V19c0 .45.108.875.3 1.25H6A2.25 2.25 0 0 1 3.75 18V4.943a.25.25 0 0 1 .395-.203l.175.125a3.76 3.76 0 0 0 4.366-.005M17.75 19v-4.75h2.5V19a1.25 1.25 0 0 1-2.5 0"
                       clip-rule="evenodd"
                     />
@@ -293,16 +327,12 @@ onUnmounted(async () => {
               </div>
               <div v-if="data.stt_id === 'RAP'" class="w-1/2">
                 <a
-                  :href="data.ticket_url"
-                  target="_blank"
+                  :href="data.ticket_url" target="_blank"
                   class="btn border-emerald-border w-full bg-emerald-400 text-sm font-medium text-white hover:border-slate-300 hover:bg-emerald-500 dark:border-emerald-700 dark:bg-emerald-500 dark:hover:bg-emerald-600"
                 >
                   <svg xmlns="http://www.w3.org/2000/svg" class="size-6 shrink-0" viewBox="0 0 24 24">
                     <path
-                      fill="none"
-                      stroke="currentColor"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
+                      fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
                       stroke-width="1.5"
                       d="M19 19a2 2 0 0 0 2-2v-3a2 2 0 1 1 0-4V7a2 2 0 0 0-2-2H5a2 2 0 0 0-2 2v3a2 2 0 1 1 0 4v3a2 2 0 0 0 2 2zm-7-7.25v.5M12 8v.5m0 7v.5"
                     />
@@ -312,8 +342,7 @@ onUnmounted(async () => {
               </div>
               <div v-else class="w-1/2">
                 <a
-                  :href="data.reg_url"
-                  target="_blank"
+                  :href="data.reg_url" target="_blank"
                   class="btn w-full border border-slate-100 bg-white text-sm font-medium text-slate-600 hover:border-slate-300 dark:border-slate-700 dark:bg-slate-800 dark:text-slate-300 dark:hover:border-slate-600"
                 >
                   <svg xmlns="http://www.w3.org/2000/svg" class="size-6 shrink-0" viewBox="0 0 24 24">
@@ -339,8 +368,7 @@ onUnmounted(async () => {
                 class="number rounded border border-slate-200 text-sm shadow-sm duration-150 ease-in-out hover:border-slate-300 dark:border-slate-700 dark:bg-slate-800 dark:hover:border-slate-600"
               >
                 <div
-                  v-for="answer in data.ans"
-                  :key="answer.id"
+                  v-for="answer in data.ans" :key="answer.id"
                   class="flex flex-col px-4 py-2 transition-colors hover:bg-slate-200/70 dark:hover:bg-slate-900"
                 >
                   <div class="text-xs text-muted-foreground dark:text-slate-500">
@@ -360,9 +388,7 @@ onUnmounted(async () => {
               <form>
                 <label class="sr-only" for="notes">Write a note</label>
                 <textarea
-                  id="notes"
-                  class="form-textarea w-full focus:border-slate-300"
-                  rows="4"
+                  id="notes" class="form-textarea w-full focus:border-slate-300" rows="4"
                   placeholder="Write a note…"
                 />
               </form>
