f07eb255e96a3b76543da6ff1219dfece12bb5f1
habibmac <mcrury@gmail.com>
2025-06-13 01:24:36 +0700

    fix: shadcn-vue dropdown checkbox item color

--- components/ui/dropdown-menu/DropdownMenuCheckboxItem.vue
+++ components/ui/dropdown-menu/DropdownMenuCheckboxItem.vue
@@ -34,7 +34,7 @@ const forwarded = useForwardPropsEmits(delegatedProps, emits);
   >
     <span class="absolute left-2 flex h-3.5 w-3.5 items-center justify-center">
       <DropdownMenuItemIndicator>
-        <CheckIcon class="w-4 h-4" />
+        <CheckIcon class="w-4 h-4 text-primary" />
       </DropdownMenuItemIndicator>
     </span>
     <slot />
