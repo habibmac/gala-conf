828aa5f54dd8b94ae40a1f1caa3cd0e02a4db80d
habibmac <mcrury@gmail.com>
2025-06-22 16:29:13 +0700

    fix: improve reg cards layout on dashboard and reg page

--- components/partials/registrations/RegCards.vue
+++ components/partials/registrations/RegCards.vue
@@ -6,6 +6,10 @@ import { Skeleton } from '@/components/ui/skeleton';
 import { cn } from '@/lib/utils';
 import { formatThousands } from '@/utils';
 
+defineProps<{
+  forDashboard?: boolean
+}>();
+
 const route = useRoute();
 const eventId = ref(route.params.eventId as string) || ref('');
 
@@ -24,60 +28,68 @@ const { data, isError, isLoading, isRefetching, refetch } = useQuery({
 </script>
 
 <template>
-  <div v-if="isLoading || isRefetching" class="grid gap-4 sm:grid-cols-2 lg:grid-cols-4 ">
-    <Skeleton v-for="i in 4" :key="i" class="h-28 rounded-xl bg-muted-foreground/10" />
+  <div
+    v-if="isLoading || isRefetching"
+    :class="cn(
+      'grid gap-4',
+      forDashboard ? 'grid-cols-2' : 'grid-cols-2 md:grid-cols-4',
+    )"
+  >
+    <Skeleton v-for="i in 4" :key="i" class="h-32 rounded-xl bg-muted-foreground/10" />
   </div>
   <div v-else-if="isError" class="py-16">
     <div class="flex h-32 items-center justify-center">
       Error fetching event summary. Please try again later.
     </div>
   </div>
-  <template v-else-if="data">
-    <div v-if="data.length > 0" class="grid gap-4 md:grid-cols-2 lg:grid-cols-4">
-      <Card v-for="item in data" :key="item.title" class="relative h-28 overflow-hidden">
-        <CardHeader class="flex flex-row items-center justify-between space-y-0 pb-2">
-          <CardTitle class="text-sm font-medium tracking-normal">
+  <div
+    v-else-if="data.length > 0"
+    :class="cn(
+      'grid gap-4',
+      forDashboard ? 'grid-cols-2' : 'grid-cols-2 md:grid-cols-4',
+    )"
+  >
+    <Card v-for="item in data" :key="item.title" class="relative h-32 overflow-hidden">
+      <CardHeader class="flex flex-row items-center justify-between space-y-0 pb-2">
+        <Button
+          variant="ghost"
+          size="icon"
+          class="group absolute right-3 top-3"
+          @click="refetch"
+        >
+          <Icon
+            icon="ri:refresh-line"
+            class="size-3.5 text-slate-400 transition-transform group-hover:rotate-180 group-hover:text-slate-800 dark:group-hover:text-slate-100"
+            :class="{ 'animate-spin': isRefetching }"
+          />
+        </Button>
+      </CardHeader>
+      <CardContent class="relative">
+        <div
+          :class="cn('text-2xl font-semibold ordinal slashed-zero tabular-nums',
+                     item.key === 'approved_total' ? 'text-emerald-500' : 'text-foreground',
+          )"
+        >
+          {{ formatThousands(item.value) }}
+          <span v-if="item.is_currency" class="text-xs font-medium text-muted-foreground"> IDR </span>
+        </div>
+        <CardTitle class="text-sm font-medium tracking-normal">
+          <NuxtLink v-if="forDashboard" :to="`/event/${eventId}/registrations`" class="hover:underline">
             {{ item.title }}
-          </CardTitle>
-          <Button
-            variant="ghost"
-            size="icon"
-            class="group absolute right-3 top-3"
-            @click="refetch"
-          >
-            <Icon
-              icon="ri:refresh-line"
-              class="size-3.5 text-slate-400 transition-transform group-hover:rotate-180 group-hover:text-slate-800 dark:group-hover:text-slate-100"
-              :class="{ 'animate-spin': isRefetching }"
-            />
-          </Button>
-        </CardHeader>
-        <CardContent class="relative">
-          <div :class="cn('text-2xl font-semibold ordinal slashed-zero tabular-nums')">
-            {{ formatThousands(item.value) }}
-            <span v-if="item.is_currency" class="text-xs font-medium text-muted-foreground"> IDR </span>
-          </div>
-          <p v-if="item.change" class="text-xs text-muted-foreground">
-            {{ item.change }}% change
-          </p>
-        </CardContent>
-      </Card>
-    </div>
-    <div v-else class="mx-auto w-full">
-      <EmptyState
-        title="No data available"
-        description="There is no data available for this event."
-        :img="{ src: '/images/empty-state/no-data.svg', alt: 'No data' }"
-      />
-    </div>
-  </template>
-  <template v-else>
-    <div class="mx-auto w-full">
-      <EmptyState
-        title="No data available"
-        description="There is no data available for this event."
-        :img="{ src: '/images/empty-state/no-data.svg', alt: 'No data' }"
-      />
-    </div>
-  </template>
+          </NuxtLink>
+          <span v-else>{{ item.title }}</span>
+        </CardTitle>
+        <p v-if="item.change" class="text-xs text-muted-foreground">
+          {{ item.change }}% change
+        </p>
+      </CardContent>
+    </Card>
+  </div>
+  <div v-else class="mx-auto w-full">
+    <EmptyState
+      title="No data available"
+      description="There is no data available for this event."
+      :img="{ src: '/images/empty-state/no-data.svg', alt: 'No data' }"
+    />
+  </div>
 </template>
